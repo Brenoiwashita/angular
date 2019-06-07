ba3eb625a53d610fe0519cf6e7dcf418989ae7cc
Breno Iwashita de Souza <biwashita@server01.foursys.local>
2019-06-07 20:10:27 -0300

    TESTE

---
+++ src/app/logado/logado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logado',
+  templateUrl: './logado.component.html',
+  styleUrls: ['./logado.component.sass']
+})
+export class LogadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
